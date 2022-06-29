d2bdcdaccde7400c2a782c98d72f1ff042c8438e
nwogu emmanuel <nwogue203@gmail.com>
2022-06-29 15:29:30 -0700

    create a c-file that print using printf func

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - starting my program
+ *
+ * Return: 0 either-way
+ */
+int main(void)
+{
+/* printf - prints word enclosed in bracket */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+/* return value: 0 */
+	return (0);
+}
