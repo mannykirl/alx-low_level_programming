29ca1ce2cdd86eb310a26f2da4246e88abac6202
nwogu emmanuel <nwogue203@gmail.com>
2022-06-29 16:16:48 -0700

    create c-file to print size of c data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* main - starting my program
+ *
+ */
+int main(void);
+{
+/**
+ * Declare functions
+ * @intTYPE - contains data type
+ */
+	int intTYPE;
+	float floatTYPE;
+	char charTYPE;
+	long int longTYPE;
+	long long int longlongTYPE;
+/**
+ * sizeof - evaluates size of a variable
+ * printf - prints value in enclosed bracket appropriately
+ */
+	printf("Size of a char: %zu byte(s)\n", sizeof(charTYPE));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intTYPE));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longTYPE));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongTYPE));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatTYPE));
+/* return - 0 */
+	return (0);
+}
