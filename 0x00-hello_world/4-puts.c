587967167123301387bcd7565ea483e3761c947d
nwogu emmanuel <nwogue203@gmail.com>
2022-06-29 14:51:17 -0700

    cfile using puts function instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
-/* initiating main-void */
-int main()
-{	
-		/*initializing string for puts func */
+/* Description: Main is assigned void */
+int main(void)
+{
+/*initializing string for puts func */
 	char mystr[] = "\"Programming is like building a multilingual puzzle";
-               
-		/* Description : puts-To print value of mystr */
-		
-	puts(mystr);
-		
-		/* return value: 0 */
-
-        return (0);
+/* Description : puts-To print value of mystr */
+	puts(mystr);		
+/* return value: 0 */
+	return (0);
 }
