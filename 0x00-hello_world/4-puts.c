edf1c05d46aca8e33313a679a148232bfade01c0
nwogu emmanuel <nwogue203@gmail.com>
2022-06-29 15:04:43 -0700

    c-file  using puts func

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
-int main(void) /* @main: Main is assigned void */
+/* @main: main is assigned void */
+int main(void)
 {
 /*initializing string for puts func */
 	char mystr[] = "\"Programming is like building a multilingual puzzle";
