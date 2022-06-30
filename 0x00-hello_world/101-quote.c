76719e2955ce80e2d5eb508aa3d878d921973f25
nwogu emmanuel <nwogue203@gmail.com>
2022-06-30 12:33:33 -0700

    create c program that output string without using printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * write - initialize write to replace printf
+ * write - initialize write to replace
  * @filedes - It is the file descriptor which has been obtained from the call to open
  * @buf - points to a character array,
  * @nbyte - number of byte
@@ -11,7 +11,7 @@
 int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-	write( 2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 	
 	return (1);
 }
