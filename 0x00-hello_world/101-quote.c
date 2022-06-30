c77ce2a7083650ebdba723245240bafeea0bd72b
nwogu emmanuel <nwogue203@gmail.com>
2022-06-30 12:44:38 -0700

    create c program that output string without using printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,11 @@
  * buf - points to a character array,
  * nbyte - number of byte
  *
- * Return - return 1
+ * Return: return 1
  */
 int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
-	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);	
 	return (1);
 }
