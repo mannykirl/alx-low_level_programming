fa4c5eed6ed39fe43aa38477f1b7ffa4219cf45c
nwogu emmanuel <nwogue203@gmail.com>
2022-07-01 06:55:39 -0700

    print alphabets using putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,15 @@
  * for = for every ch <= print body
  *
  * Return: always 0
- */ 
+ */
 int main(void)
 {
 	int ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar (ch);
 	}
+	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,6 @@ int main (void)
 	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar(ch);
-		putchar('\n');
 	}
 	return (0);
 }
