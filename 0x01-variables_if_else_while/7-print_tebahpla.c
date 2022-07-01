507eb9e8339434f2a9d41eb13a4b13bae3312271
nwogu emmanuel <nwogue203@gmail.com>
2022-07-01 15:13:06 -0700

    reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - starting programs
+ * int ch - declare ch
+ * for - loop function
+ *
+ * Return: alwayys 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch <= 'a'; ch--)
+	{
+		putchar (ch);
+	}
+	putchar ('\n');
+	return (0);
+}
