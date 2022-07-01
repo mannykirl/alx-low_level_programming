f3727a362feabbe771cc374359c818abd5aca35c
nwogu emmanuel <nwogue203@gmail.com>
2022-07-01 05:26:49 -0700

    my readme

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,13 +11,16 @@
  * if - if arg is true execute body
  *
  * Return: always 0
- */ 
+ */
 int main(void)
 
 {
 	int n;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
+
 /* your code goes there */
 	if (n > 0)
 	{
