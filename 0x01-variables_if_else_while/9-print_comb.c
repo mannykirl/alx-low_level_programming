333483df205ac5f5b17decebc7e3da6d99e9411a
nwogu emmanuel <nwogue203@gmail.com>
2022-07-02 06:22:25 -0700

    print single digits with comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,9 +10,13 @@ int main(void)
 {
 	int num;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar (num);
+		putchar ((num % 10) + '0');
+	if (num == 9)
+	{
+		break;
+	}	
 		putchar (',');
 		putchar (' ');
 	}
