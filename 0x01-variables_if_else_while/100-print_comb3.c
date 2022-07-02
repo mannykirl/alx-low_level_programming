a8268e7e8a7b51d32cc39970cd150d8218428ef0
nwogu emmanuel <nwogue203@gmail.com>
2022-07-02 07:13:03 -0700

    print single digits with comma

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - starting program
+ * int i - declare i
+ * int j - declare j
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, digit;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 8 && digit == 9)
+			{
+				break;
+			}
+		for (digit = i; digit <= 9; digit++)
+		{
+		if (i == digit)
+		{
+			continue;
+		}
+			putchar ((i % 10) + '0');
+			putchar ((digit % 10) + '0');
+		if (i == 8 && digit == 9)
+		{
+			break;
+		}
+			putchar (',');
+			putchar (' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
