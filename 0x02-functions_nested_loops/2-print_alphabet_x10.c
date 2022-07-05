9a17919670588c837960ae125b11e62641dd956d
nwogu emmanuel <nwogue203@gmail.com>
2022-07-05 09:59:39 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet x10
+ */
+void print_alphabet_x10(void)
+{
+	int new = 0
+	char ch;
+
+	while (new <= 9)
+	{
+		for (ch = 97; ch <= 122; ch++)
+		{
+			_putchar(ch);
+		}
+	}
+	_putchar('\n');
+}
