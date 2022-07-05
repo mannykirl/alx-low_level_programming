757116c025eed34abe0038ca5d21282e83aa6230
manny <nwogue203@gmail.com>
2022-07-05 14:30:51 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/** add - add two variables
+ * @int 1: var 1
+ * @int 2: var 2
+ *
+ * Return: result of int 1 + 2
+ */
+int add(int 1, int 2)
+{
+	result = int 1 + int 2;
+
+	return (result);
+}
