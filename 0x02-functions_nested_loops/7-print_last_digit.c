4d63018d4ea360ca21530dd4e6197609e3842abb
manny <nwogue203@gmail.com>
2022-07-05 13:10:36 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,12 +3,12 @@
  * print_last_digit - print last digit of value in var
  * @n: variable
  *
- * Return: always 0
+ * Return: always lastdigit
  */
 int print_last_digit(int n)
 {
 	int lastdigit = n % 10;
 
 	_putchar(lastdigit + '0');
-	return (0);
+	return (lastdigit);
 }
