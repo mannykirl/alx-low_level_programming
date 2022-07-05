79f068724ef21aa09ba2b263c131e7aaff0eb205
nwogu emmanuel <nwogue203@gmail.com>
2022-07-05 09:31:28 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,19 @@
 #include "main.h"
-
+/**
+ * main - start program to print putchar
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-	_putchar(_);
-	_putchar(p);
-	_putchar(u);
-	_putchar(t);
-	_putchar(c);
-	_putchar(h);
-	_putchar(a);
-	_putchar(r);
+	_putchar(95);
+	_putchar112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
 	_putchar('\n');
 	return (0);
 }
