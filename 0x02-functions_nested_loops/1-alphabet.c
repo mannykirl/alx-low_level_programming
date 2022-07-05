1b10b0db4dc2cd6ac691de999eb7a1123ad9bc80
nwogu emmanuel <nwogue203@gmail.com>
2022-07-05 09:48:27 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
  * print_alphabet - print alphabet a-z
- *
- * Return: always 0
  */
 void print_alphabet(void)
 {
 	char ch;
-	for (ch = "a"; ch <= "z"; ch++)
+	for (ch = "97"; ch <= "112"; ch++)
 	{
 		_putchar(ch);
 	}
 	_putchar('\n');
-	return (0);
 }
