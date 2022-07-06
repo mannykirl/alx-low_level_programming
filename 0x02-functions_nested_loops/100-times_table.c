a362c3fdbb7e9517678015ff6b7a516554e54d1b
manny <nwogue203@gmail.com>
2022-07-06 14:35:45 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,7 +16,7 @@ void print_times_table(int n)
 	while (a <= n)
 	{
 		for (b = 0; b <= n; b++)
-	{
+		{
 		rep = a * b;
 		if (b == 0)
 		{
@@ -45,8 +45,8 @@ void print_times_table(int n)
 			_putchar(',');
 			_putchar(' ');
 		}
-	}
-	_putchar('\n');
-	a++;
+		}
+		_putchar('\n');
+		a++;
 	}
 }
