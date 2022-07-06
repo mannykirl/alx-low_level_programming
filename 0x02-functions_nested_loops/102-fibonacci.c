b1c794e74b4641847b80cdab12e0a4d0d1ac9a63
manny <nwogue203@gmail.com>
2022-07-05 18:23:28 -0700

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - start
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a = 0, b = 1, next;
+	int i;
+	
+	for (i = 0; i <= 50; i++)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+
+		printf("%i, ", b);
+	}
+	putchar('\n');
+	return (0);
+}
