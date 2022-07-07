52b6abc45dda3e4c4d9f12455f013ca980328251
manny <nwogue203@gmail.com>
2022-07-07 13:31:35 -0700

    hope u work

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+	int c, k, p;
+
+	for (c = 1; c <= 10; c++)
+	{
+		for (k = 0; k <= 14; k++)
+		{
+			if (p = c * k)
+				_putchar(p);
+		}
+	}
+	_putchar('\n')
+}
