12168c4c43c6929f2cdc7842a8b45d7e62712180
manny <nwogue203@gmail.com>
2022-07-07 01:15:23 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isupper - return 1 for uppercase char
+ * @c: c is checked for upper case
+ *
+ * Return: return 0 if condition not met
+ */
+int _isupper(int c)
+{
+	int c;
+	
+	if (c = A; c <= Z; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
