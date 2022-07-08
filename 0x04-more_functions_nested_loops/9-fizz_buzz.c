381b16faa49719b61b811a3fbc88abc4edfcc484
manny <nwogue203@gmail.com>
2022-07-08 08:18:02 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - start buzz test
  * @i: check i for number
  *
  * Return: return 0
  */
+
 int main(void)
 {
 	int i;
@@ -13,13 +15,13 @@ int main(void)
 	{
 		putchar(i);
 		putchar(' ');
+	}
 	if (i % 3 == 0)
 		printf("Fizz");
 	if(i % 5 == 0)
 		printf("Buzz");
 	if (i % 5 == 0 || i % 3 == 0)
 		printf("FizzBuzz");
-	}
 	putchar('\n');
 	return (0);
 }
