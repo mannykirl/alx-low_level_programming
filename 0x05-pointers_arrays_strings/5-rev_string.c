f1948fd2694e1683030e8a68bb54561e869c46e6
manny <nwogue203@gmail.com>
2022-07-12 09:31:17 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void rev_string(char *s)
+{
+	int i, j, len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	j = len - 1;
+
+	for (i = 0; i < len; i++)
+	{
+		rev[i] = str[j];
+		j--;
+	}
+}
