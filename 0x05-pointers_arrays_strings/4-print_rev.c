c6827398ed8139bcb260631556b7175e98c5cc4f
manny <nwogue203@gmail.com>
2022-07-12 03:24:23 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: contains string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		--s;
+	}
+	_putchar('\n');
+}
