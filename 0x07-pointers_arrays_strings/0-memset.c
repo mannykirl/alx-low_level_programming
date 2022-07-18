70d2a938ad3a54f3d8f24d803474be9f418ebfec
manny <nwogue203@gmail.com>
2022-07-18 05:28:03 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset -  assign/fill a memory with constant byte
+ * @s: void pointer that contains destination
+ * @b: char to be assigned
+ * @n: number of byte to be filled
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned char *p = s;
+
+	while (n--)
+	{
+		*p++ = char b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This repo contains ALX task on 0x07-pointers_arrays_strings
