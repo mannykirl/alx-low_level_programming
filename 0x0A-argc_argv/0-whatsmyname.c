d5f7a59e2f1755a0567afcce69bd5ec9ab464a4a
manny <nwogue203@gmail.com>
2022-07-24 07:05:16 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print name of program
+ * @argc: size if array of string
+ * @argv: array of string
+ *
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
