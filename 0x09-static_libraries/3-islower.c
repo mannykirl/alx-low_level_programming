dda9706a6e664366f186e36dcaa7ba3c3c5947fa
manny <nwogue203@gmail.com>
2022-07-24 06:00:17 -0700

    task 0

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - return 1 for lowercase and otherwise
+ * @c: arg
+ *
+ * Return: 1 if lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
